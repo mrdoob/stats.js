b3a2a52169cef767c69f1ac3051a90a87897d209
Amir Ebrahimi <github@aebrahimi.com>
2015-06-18 11:58:47 -0700

    Better fix for mobile Safari by using self.performance, which doesn't throw a ReferenceError

--- src/Stats.js
+++ src/Stats.js
@@ -4,12 +4,7 @@
 
 var Stats = function () {
 
-	// AE: Fix for mobile Safari's ReferenceError
-	if (typeof(performance) === 'undefined') {
-		performance = null;
-	}
-
-	var now = ( performance && performance.now ) ? performance.now.bind( performance ) : Date.now;
+	var now = ( self.performance && self.performance.now ) ? self.performance.now.bind( performance ) : Date.now;
 
 	var startTime = now(), prevTime = startTime;
 	var frames = 0, mode = 0;
@@ -97,7 +92,7 @@ var Stats = function () {
 
 	// MEM
 
-	if ( performance && performance.memory ) {
+	if ( self.performance && self.performance.memory ) {
 
 		var mem = 0, memMin = Infinity, memMax = 0;
 
@@ -152,7 +147,7 @@ var Stats = function () {
 				prevTime = time;
 				frames = 0;
 
-				if ( mem !== undefined ) {
+				if ( mem !== undefined && self.performance ) {
 
 					var heapSize = performance.memory.usedJSHeapSize;
 					var heapSizeLimit = performance.memory.jsHeapSizeLimit;
