912c2723dd65ee6cc66d1df89bd8527297e376eb
Mr.doob <info@mrdoob.com>
2016-04-11 08:07:15 +0100

    window.devicePixelRatio support

--- src/Stats.js
+++ src/Stats.js
@@ -7,11 +7,10 @@ var Stats = function () {
 	var mode = 0;
 
 	var container = document.createElement( 'div' );
-	container.style.cssText = 'opacity:0.9;cursor:pointer';
+	container.style.cssText = 'cursor:pointer;opacity:0.9';
 	container.addEventListener( 'click', function ( event ) {
 
 		event.preventDefault();
-
 		showPanel( ++ mode % container.children.length );
 
 	}, false );
@@ -84,7 +83,7 @@ var Stats = function () {
 				prevTime = time;
 				frames = 0;
 
-				if ( memPanel !== undefined ) {
+				if ( memPanel ) {
 
 					var memory = performance.memory;
 					memPanel.update( memory.usedJSHeapSize / 1048576, memory.jsHeapSizeLimit / 1048576 );
@@ -109,25 +108,33 @@ var Stats = function () {
 
 Stats.Panel = function ( name, fg, bg ) {
 
+	var min = Infinity, max = 0, round = Math.round;
+	var PR = round( window.devicePixelRatio || 1 );
+
+	var WIDTH = 80 * PR, HEIGHT = 48 * PR,
+			TEXT_X = 3 * PR, TEXT_Y = 2 * PR,
+			GRAPH_X = 3 * PR, GRAPH_Y = 15 * PR,
+			GRAPH_WIDTH = 74 * PR, GRAPH_HEIGHT = 30 * PR;
+
 	var canvas = document.createElement( 'canvas' );
-	canvas.width = 80;
-	canvas.height = 48;
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+	canvas.style.cssText = 'width:80px;height:48px';
 
 	var context = canvas.getContext( '2d' );
+	context.font = 'bold ' + ( 9 * PR ) + 'px Helvetica,Arial,sans-serif';
+	context.textBaseline = 'top';
 
 	context.fillStyle = bg;
-	context.fillRect( 0, 0, 80, 48 );
+	context.fillRect( 0, 0, WIDTH, HEIGHT );
 
-	context.font = 'bold 9px Helvetica,Arial,sans-serif';
 	context.fillStyle = fg;
-	context.fillText( name, 3, 10 );
-	context.fillRect( 3, 15, 74, 30 );
+	context.fillText( name, TEXT_X, TEXT_Y );
+	context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
 
 	context.fillStyle = bg;
 	context.globalAlpha = 0.9;
-	context.fillRect( 3, 15, 74, 30 );
-
-	var min = Infinity, max = 0, round = Math.round;
+	context.fillRect( GRAPH_X, GRAPH_Y, GRAPH_WIDTH, GRAPH_HEIGHT );
 
 	return {
 
@@ -138,19 +145,19 @@ Stats.Panel = function ( name, fg, bg ) {
 			min = Math.min( min, value );
 			max = Math.max( max, value );
 
-			context.globalAlpha = 1;
 			context.fillStyle = bg;
-			context.fillRect( 0, 0, 80, 15 );
+			context.globalAlpha = 1;
+			context.fillRect( 0, 0, WIDTH, GRAPH_Y );
 			context.fillStyle = fg;
-			context.fillText( round( value ) + ' ' + name + ' (' + round( min ) + '-' + round( max ) + ')', 3, 10 );
+			context.fillText( round( value ) + ' ' + name + ' (' + round( min ) + '-' + round( max ) + ')', TEXT_X, TEXT_Y );
 
-			context.drawImage( canvas, 4, 15, 74, 30, 3, 15, 74, 30 );
+			context.drawImage( canvas, GRAPH_X + PR, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT, GRAPH_X, GRAPH_Y, GRAPH_WIDTH - PR, GRAPH_HEIGHT );
 
-			context.fillRect( 76, 15, 1, 30 );
+			context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, GRAPH_HEIGHT );
 
 			context.fillStyle = bg;
 			context.globalAlpha = 0.9;
-			context.fillRect( 76, 15, 1, 30 - round( ( value / maxValue ) * 30 ) );
+			context.fillRect( GRAPH_X + GRAPH_WIDTH - PR, GRAPH_Y, PR, round( ( 1 - ( value / maxValue ) ) * GRAPH_HEIGHT ) );
 
 		}
 
