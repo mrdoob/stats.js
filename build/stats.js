2822d140344769168dcb27d8d3dacba01091db86
munrocket <munrocket@protonmail.com>
2019-08-17 01:26:01 +0400

    rebuild

--- build/stats.js
+++ build/stats.js
@@ -2,7 +2,8 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global = global || self, global.Stats = factory());
-}(this, function () {
+}(this, function () { 'use strict';
+
 	/**
 	 * @author mrdoob / http://mrdoob.com/
 	 */
